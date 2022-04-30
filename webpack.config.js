26e35c05bbf28e7744d7b7c626783db7f1c140c2
Mike <ampgzo2@gmail.com>
2022-04-30 11:52:49 -0400

    create manifest.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
 // BundleAnaylyzerPlugin will analyze our bundle sizes and see how much Javascript is being processed in the browser.
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
@@ -68,6 +69,22 @@ module.exports = {
         }),
         new BundleAnalyzerPlugin({
             analyzerMode: "static", // the report outputs to an HTML file in the dist folder.
+        }),
+        // new keyword is invoking a constructor function
+        new WebpackPwaManifest({
+            name: "Food Event",
+            short_name: "Foodies",
+            description: "An app that allows you to view upcoming food events.",
+            start_url: "../index.html",
+            background_color: "#01579b",
+            theme_color: "#ffffff",
+            fingerprints: false,
+            inject: false,
+            icons: [{
+              src: path.resolve("assets/img/icons/icon-512x512.png"),
+              sizes: [96, 128, 192, 256, 384, 512],
+              destination: path.join("assets", "icons")
+            }]
         })
     ],
 
