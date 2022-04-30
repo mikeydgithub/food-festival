57553ce7a4e5c7e1c8b14bd2f7caed90f8f6c1bf
Mike <ampgzo2@gmail.com>
2022-04-30 11:22:50 -0400

    fetch listener request for service worker

--- service-worker.js
+++ service-worker.js
@@ -54,3 +54,29 @@ self.addEventListener('activate', function(e) {
         })
     )
 })
+
+// retrieve infromation from the cahe
+// listen for the fetch event, log the URL of the requested resource, and then define how to respond to the request.
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    // respondWith() to intercept the fetch request.
+    e.respondWith(
+        // .match() to determine if the resource already exists in caches.
+        caches.match(e.request).then(function (request) {
+            if (request) { 
+                // if cache is available, respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+                // if the resource is not in the caches, we allow the resources to be retrieved from the online network as usual.
+            } else { 
+                // if there are no caches, try fetching request.
+                console.log('file is not cached, fetching: ' + e.request.url)
+                return fetch(e.request)
+            }
+            // You can omit if/else for console.log & put one line below like this too.
+            // return request || fetch(e.request) 
+            // if the file isn't stored in the cache, the service worker will make a normal request for the resource.
+        })
+
+    )
+})
